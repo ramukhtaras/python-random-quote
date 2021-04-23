524baf15576c308c441dfaf5ee48e9c382d1fd43
ramukhtaras <sarathkumartr1997@gmail.com>
2021-04-23 21:55:53 +0530

    Explore python by your own #4

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,21 @@
 import random
+
+
+
 def primary():
   #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
+  quotes.append("Make hay while sunshine")
   f.close()
 
   last= len(quotes)-1
   rnd= random.randint(0, last)
+  arnd= random.randint(0, last)
 
-  print(quotes[rnd])
+  print(f'{quotes[rnd]}{quotes[arnd]}')
+  #print(quotes[arnd])
 
 if __name__== "__main__":
   primary()
